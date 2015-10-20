dfea29c6a83b9de068a002dd1dbce2eec363f94f
CNA-Bld <cna.bld@gmail.com>
2015-10-20 23:20:20 +0800

    enable gzip

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask.ext.compress import Compress
 
 app = Flask(__name__)
 api = Api(app, prefix='/api')
@@ -7,3 +8,5 @@ api = Api(app, prefix='/api')
 app.config.from_object('config')
 
 from app import views
+
+Compress(app)
